7ecb5f9e7c999233e0e674e41a7964966961883d
Jignesh Vaghasiya <jignesh.vaghasiya@vnc.biz>
2012-02-23 06:48:07 +0000

    After creating symbolic link.

--- instalationopenerp.sh
+++ instalationopenerp.sh
@@ -23,7 +23,9 @@ OPEN_ERP_ZIMLET=$EXTRACTED_DIR"com_zimbra_erp_mail_connector.zip"
 DB_FILE=$EXTRACTED_DIR"db.sql"
 JSON_JAR_FILE=$EXTRACTED_DIR"json_simple-1.1.jar"
 JSON_JAR_FILE_LOCAL="/opt/zimbra/mailboxd/webapps/zimlet/WEB-INF/lib/"
-
+LINK1="zimbrastore.jar"
+LINK2="zimbrasoap.jar"
+LINK3="zimbracommon.jar"
 echo ""
 
 do_run tar xvzf $ZIPFILE -C $WORK_DIR 
@@ -36,6 +38,26 @@ do_run cp -f $LIB $JSON_JAR_FILE_LOCAL
 echo -n "Deploying zimlets...."
 do_run zmzimletctl deploy $OPEN_ERP_ZIMLET
 echo "Done"
+
+
+do_run cd $JSON_JAR_FILE_LOCAL
+if [ ! -L "$LINK1" ] ; then
+	do_run ln -s /opt/zimbra/lib/jars/zimbrastore.jar zimbrastore.jar
+else
+	echo ""
+fi;
+if [ ! -L "$LINK2" ] ; then
+	do_run ln -s /opt/zimbra/lib/jars/zimbrasoap.jar zimbrasoap.jar
+else
+        echo ""
+fi; 
+
+if [ ! -L "$LINK3" ] ; then
+	do_run ln -s /opt/zimbra/lib/jars/zimbracommon.jar zimbracommon.jar
+else
+        echo ""
+fi; 
+
 
 echo -n "Do you want to restart mailbox service?[Y/N]:"
 read ans
