722885c7cb12fe9887cbf1ec21730d8e2a82fda6
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2012-08-14 23:06:41 +0200

    retrieving version information from autoprocessed translation files

--- src/com_zimbra_erp_mail_connector/src/about_setting.js
+++ src/com_zimbra_erp_mail_connector/src/about_setting.js
@@ -1,6 +1,9 @@
 /* This file contains about information */
+
 about_setting.prototype = new DwtTabViewPage;
+
 about_setting.prototype.constructor = about_setting;
+
 var zm;
 
 function about_setting(parent, zimlet){
@@ -21,7 +24,7 @@ about_setting.prototype._createHTML = function() {
 	html[i++]="<div id='logo' style='width:100%;height:50%;margin-left:80px;'><img src='"+zm.getResource("resources/VNC-Logo.png")+"'/></div>";
 	html[i++]="</td>";
 	html[i++]="<tr><td><font face='Times New Roman' size='5px'>VNC OpenERP Zimlet (VOZ)</font></td></tr>";
-	html[i++]="<tr><td><b><center>Version 1.0 Pre-Alpha</center></b></td></tr>";
+	html[i++]="<tr><td><b><center>"+zm.getMessage("ZIMLET_VERSION")+"</center></b></td></tr>";
 	html[i++]="</tr>";
 	html[i++]="</table>";
 	this.getHtmlElement().innerHTML = html.join("");
