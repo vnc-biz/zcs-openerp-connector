8226a889ad8c0f8e56f1da517bc9dc3daba504f1
Jignesh Vaghasiya <jignesh.vaghasiya@vnc.biz>
2012-08-16 14:34:47 +0530

    Added copyright information and tested version information #4356

--- src/com_zimbra_erp_mail_connector/src/about_setting.js
+++ src/com_zimbra_erp_mail_connector/src/about_setting.js
@@ -22,10 +22,10 @@ about_setting.prototype._createHTML = function() {
 	html[i++]="<tr>";
 	html[i++]="<td>";
 	html[i++]="<div id='logo' style='width:100%;height:50%;margin-left:80px;'><img src='"+zm.getResource("resources/VNC-Logo.png")+"'/></div>";
-	html[i++]="</td>";
+	html[i++]="</td></tr>";
 	html[i++]="<tr><td><font face='Times New Roman' size='5px'>VNC OpenERP Zimlet (VOZ)</font></td></tr>";
 	html[i++]="<tr><td><b><center>"+zm.getMessage("ZIMLET_VERSION")+"</center></b></td></tr>";
-	html[i++]="</tr>";
+	html[i++]="<tr><td><div class='voz_lincense'>http://www.vnc.biz </div><div>Copyright 2012, VNC - Virtual Network Consult GmbH </div><div class='voz_lincense2'>Released under GPL Licenses. </div></font></center></td></tr>";
 	html[i++]="</table>";
 	this.getHtmlElement().innerHTML = html.join("");
 }
